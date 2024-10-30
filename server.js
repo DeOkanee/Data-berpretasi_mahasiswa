d7dafbb640e685dddcf5d4122412e61989470efb
De Oka <115537848+DeOkanee@users.noreply.github.com>
2024-10-30 12:40:33 +0800

    update code

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
@@ -14,6 +13,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
+
+// Routes untuk Akademik-2024
+app.get('/Akademik-2024', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'Akademik-2024', 'akademik-2024.html'));
+});
+
+app.get('/Akademik-2024/upload', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'Akademik-2024', 'upload.html'));
+});
+
+// Serve static files untuk Akademik-2024
+app.use('/Akademik-2024', express.static(path.join(__dirname, 'public', 'Akademik-2024')));
 
 app.get('/akademik-2023', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'akademik-2023.html'));
@@ -34,7 +45,12 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-// Start server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// Vercel specific
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
